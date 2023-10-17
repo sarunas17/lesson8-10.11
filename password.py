f578ec590a43c42662089bb46670e23556f5c790
Sarunas Staskevicius <sarunas.staskevicius@gmail.com>
2023-10-17 17:44:38 +0300

    add code improve

--- password.py
+++ password.py
@@ -8,28 +8,46 @@
 # If in password numbers or symbols are not provided, the program should add additional value to dictionary as a value, names 'improved password'
 
 input_splited = []
-while len(input_splited) < 10:
- user_input = input("Enter minimum 5 usernames and passwords in the format username= , password= ; : ")
- input_replaced = user_input.replace(";", ",").replace(" ","")
+while len(input_splited) < 20:
+ user_input = input("Enter minimum 5 usernames and passwords in the format username='' , password=''; : ")
+ input_replaced = user_input.replace(" ","").replace("'","").replace(";",",").replace("=",",")
  input_splited = input_replaced.split(",")
 
-list= []
-for value in input_splited:
-    list.append(value[9:])
-    list_username = list[::2]
-    list_password = list[1::2]
+list = []
+list = input_splited[1::2]
+list_username = list[::2]
+list_password = list[1::2]
+user_data = dict(zip(list_username, list_password))
+
+
 
 attempts = 1
 
 while True:
-    
-    username = input("Enter your username: ")
-    password = input("Enter your password: ")
-    if username in list_username and password in list_password:
-        print("Welcome")
-        break
-    else: 
-        print(f"Incorrect username or password. Please try again. Number of attempts: {attempts}")
-        attempts = attempts + 1
+   username_input = input("Enter your username: ")
+   password_input = input("Enter your password: ")
+   for username, password in user_data.items():
+        if username == username_input and password == password_input:
+          print("Welcome")
+          break
+   else: 
+          print(f"Incorrect username or password. Please try again. Number of attempts: {attempts}")
+          attempts = attempts + 1
+          continue
+   break
+          
+
+# has_numbers = any(char.isdigit() for char in password)
+# has_symbols = any(char in "!@#$%^&*()-_+=<>?[]{}" for char in password)
+
+# if not has_numbers or not has_symbols:
+#     if not has_numbers:
+#         password += '123'  
+#     if not has_symbols:
+#         password += '!@#'  
+
+# print("Improved Password:", password)
+
+                  
 
                   
